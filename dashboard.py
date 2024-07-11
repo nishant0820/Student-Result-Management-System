e4a15048426fb4b8f98d5a989f25c03ed25318f1
Nishant Gupta <gupta.nishant08042004@gmail.com>
2024-07-11 19:27:24 +0530

    created dashboard.py file

--- dashboard.py
+++ dashboard.py
@@ -24,8 +24,23 @@ class RMS:
         btn_view=Button(M_Frame,text="View Result",font=("goudy old style",15,"bold"),bg="#0b5377",fg="white",cursor="hand2").place(x=800,y=5,width=250,height=40)
         btn_exit=Button(M_Frame,text="Exit",font=("goudy old style",15,"bold"),bg="#0b5377",fg="white",cursor="hand2").place(x=1060,y=5,width=250,height=40)
 
+        #---------- content window ----------------
+        self.bg_img=Image.open("Student-Result-Management-System/images/bg.png")
+        self.bg_img=self.bg_img.resize((920,350))
+        self.bg_img=ImageTk.PhotoImage(self.bg_img)
+
+        self.lbl_bg=Label(self.root,image=self.bg_img).place(x=400,y=180,width=920,height=350)
+
+        #-------------- update details ----------------
+        self.lbl_course=Label(self.root,text="Total Courses\n[ 0 ]",font=("goudy old style",20),bd=10,relief=RIDGE,bg="#e43b06",fg="white")
+        self.lbl_course.place(x=400,y=540,width=300,height=100)
+        self.lbl_student=Label(self.root,text="Total Students\n[ 0 ]",font=("goudy old style",20),bd=10,relief=RIDGE,bg="#0676ad",fg="white")
+        self.lbl_student.place(x=710,y=540,width=300,height=100)
+        self.lbl_result=Label(self.root,text="Total Results\n[ 0 ]",font=("goudy old style",20),bd=10,relief=RIDGE,bg="#038074",fg="white")
+        self.lbl_result.place(x=1020,y=540,width=300,height=100)
+
         #------------- footer --------------
-        footer=Label(self.root,text="SRMS-",compound=LEFT,padx=10,image=self.logo_dash,font=("goudy old style",20,"bold"),bg="#033054",fg="white").place(x=0,y=0,relwidth=1,height=50)
+        footer=Label(self.root,text="Student Result Management System\nContact Us for any Technical Issue: 9899459288",font=("goudy old style",12),bg="#262626",fg="white").pack(side=BOTTOM,fill=X)
 
 
 
