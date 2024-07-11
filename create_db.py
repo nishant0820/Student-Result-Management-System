5c4bf2bd9f616224b62029867c801d7f913a36e2
Nishant Gupta <gupta.nishant08042004@gmail.com>
2024-07-11 21:53:21 +0530

    create course table database file

---
+++ create_db.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+def create_db():
+    con=sqlite3.connect(database="rms.db")
+    cur=con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS course(cid INTEGER PRIMARY KEY AUTOINCREMENT,name text,duration text,charges text,description text)")
+create_db()
